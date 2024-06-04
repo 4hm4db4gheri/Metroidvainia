5070dc0f56aa25da14ffa292a0cfc0f53159cd15
Ahmad Bagheri <u.ahmadbagheri@gmail.com>
2024-06-04 19:19:30 +0330

    Camera grids completed

--- Assets/Scripts/Camera Control.cs
+++ Assets/Scripts/Camera Control.cs
@@ -1,36 +1,69 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
    [SerializeField] private Vector2 offset;
    [SerializeField] private Transform target;
-   [SerializeField] float cameraXGrid;
+   [SerializeField] float cameraXFrontGrid;
+   [SerializeField] float cameraXBehindGrid;
    [SerializeField] float cameraYGrid;
+
+   // private void Awake()
+   // {
+   //    Vector3 pos = target.position;
+   //    pos.x += offset.x;
+   //    pos.y = offset.y;
+   //    pos.z = -10;
+   //    transform.position = pos;
+   // }
 
    private void Update()
    {
-      // Vector3 pos = target.position;
-      // pos.x += offset.x;
-      // pos.y = offset.y;
-      // pos.z = -10;
-      // transform.position = pos;
       Vector3 pos = transform.position;
-      if ((target.position.x - transform.position.x) > cameraXGrid / 2)
+      if ((target.position.x - transform.position.x) > cameraXFrontGrid)
       {
-         pos.x += target.position.x - transform.position.x - cameraXGrid / 2;
-         transform.position = pos;
+         pos.x += target.position.x - transform.position.x - cameraXFrontGrid;
+         // transform.position = pos;
       }
-      else if ((target.position.x - transform.position.x) < -cameraXGrid / 2)
+      else if ((target.position.x - transform.position.x) < -cameraXBehindGrid)
       {
-         pos.x += target.position.x - transform.position.x + cameraXGrid / 2;
-         transform.position = pos;
+         pos.x += target.position.x - transform.position.x + cameraXBehindGrid;
+         // transform.position = pos;
       }
+      pos.y = target.position.y + offset.y;
+      transform.position = pos;
+      //  else if ((target.position.y - transform.position.y) > cameraYGrid / 2)
+      // {
+      //    pos.y += target.position.y - transform.position.y - cameraYGrid / 2;
+      //    transform.position = pos;
+      // }
+      // else if ((target.position.y - transform.position.y) < -cameraYGrid / 2)
+      // {
+      //    pos.y += target.position.y - transform.position.y + cameraYGrid / 2;
+      //    transform.position = pos;
+      // }
+      
 
    }
-   private void OnDrawGizmos()
-   {
+   // private void OnDrawGizmos()
+   // {
+   //    Gizmos.color = Color.red;
+   //    Gizmos.DrawWireCube(transform.position, new Vector2(cameraXFrontGrid, cameraYGrid));
+   // }
+   private void OnDrawGizmos() {
+
       Gizmos.color = Color.red;
-      Gizmos.DrawWireCube(transform.position, new Vector2(cameraXGrid, cameraYGrid));
+      
+      Vector3 posFront = transform.position;
+      posFront.x += cameraXFrontGrid;
+      posFront.y += 2;
+      Gizmos.DrawLine(posFront, new Vector2(posFront.x, posFront.y-6));
+
+      Vector3 posBehind = transform.position;
+      posBehind.x -= cameraXBehindGrid;
+      posBehind.y += 2;
+      Gizmos.DrawLine(posBehind, new Vector2(posBehind.x, posBehind.y-6));
    }
 }
 
